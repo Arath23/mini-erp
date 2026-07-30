3f935c7cbf979ec5f21514267db160780e53b894
Arath Daniel <anoriega@ficachisoluciones.mx>
2026-07-30 16:19:44 -0600

    Renombra AddEmployer a Main

--- src/main/java/com/arath/minierp/repository/Main.java
+++ src/main/java/com/arath/minierp/repository/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import com.arath.minierp.model.Employee;
 import com.arath.minierp.model.Employee.Genero;
 
-public class AddEmployer {
+public class Main {
 
     public static void main(String[] args) {
         
